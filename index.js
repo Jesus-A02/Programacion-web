dd598935f2ec1bf0a4d079a3d2193cc785a9120b
Jesus-A02 <pillicuentadoz@gmail.com>
2024-12-09 22:59:40 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParse = require("body-parser");
 
 const app = express();
-const PORT = process.env.PORT || 3977;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParse.urlencoded({ extended: true }));
 app.use(bodyParse.json());
